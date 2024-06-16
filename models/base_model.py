197e2f58cafb218bdd647c49e5ea8783921cc648
BoakyePrincella <princellaboakye2003@gmail.com>
2024-06-16 20:45:08 +0000

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ The string representation """
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """ The save method """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Returns dictionary after the instance is called """
