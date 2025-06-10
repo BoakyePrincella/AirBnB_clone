699346782d7d49245d9ba5349b23280d35b260b3
BoakyePrincella <boakyeprincella2003@gmail.com>
2025-06-10 15:52:21 +0000

    AirBnB Consolnsole

--- console.py
+++ console.py
@@ -2,21 +2,10 @@
 """This is entry point of the command interpreter"""
 
 import cmd
-from models.base_model import BaseModel
-from models import storage
-#from models.user import User
-from models.state import State
-from models.amenity import Amenity
-from models.review import Review
-from models.place import Place
-from models.city import City
-import re
 
 class HBNBCommand(cmd.Cmd):
     """The HBNB command"""
     prompt = '(hbnb) '
-    models_list = ["BaseModel", "User", "State", "City",
-                   "Amenity", "Place", "Review"]
 
     def do_quit(self, line):
         """Quit command to exit the program"""
@@ -30,14 +19,9 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to close the program"""
         return True
 
-    def do_create(self, line):
-        """Creates a new instance of the Basemodel and prints it id"""
-        if not line:
-            print("** class name is missing **")
-            return
-
-        line = line.split()
-        class_name = line[0]
+    def emptyline(self):
+        """Do nothing on an empty line"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" This is the base module of the AirBnB project """
-
+""" A class BaseModel that defines all common methods for other classes """
 
 import uuid
 from datetime import datetime
@@ -8,17 +7,18 @@ from models import storage
 
 
 class BaseModel:
-    """ The BaseModel class """
+    """This is the BaseModel for all the classes"""
+
     def __init__(self, *args, **kwargs):
-        """ The initialisation method """
+        """The initializer of the class"""
         if kwargs:
-            for key,value in kwargs.items():
-                if not key.startswith("__"):
-                    if key == "updated_at" or key == "created_at":
+            for k, v in kwargs.items():
+                if not k.startswith("__"):
+                    if k == "created_at" or k == "updated_at":
                         date_format = "%Y-%m-%dT%H:%M:%S.%f"
-                        self.__dict__[key] = datetime.strptime(value, date_format)
+                        self.__dict__[k] = datetime.strptime(v, date_format)
                     else:
-                        self.__dict__[key] = value
+                        self.__dict__[k] = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -26,17 +26,17 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """ The string representation """
-        return ("[{}] ({}) {}"
+        """The string representation of the object format"""
+        return ("[{} ({}) {}]"
                 .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """ The save method """
+        """updates the attribute `updated_at` with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """ Returns dictionary after the instance is called """
+        """returns a dictionary after the instane is called"""
         dictionary = dict(self.__dict__)
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = self.created_at.isoformat()
